febd89e2f60247f13205a05eaaf1487a59a8a0ac
MohamedVal <med4en@gmail.com>
2021-07-30 01:34:42 +0000

    finished the home page UI

--- index.js
+++ index.js
@@ -34,5 +34,39 @@ $(document).ready(function() {
     $(".button-group").on("click", "button", function(){
         var filterValue = $(this).attr('data-filter');
         $grid.isotope({filter:filterValue});
-    })
+    });
+
+    
+    //new phones carousel 
+    $("#new-phones .owl-carousel").owlCarousel({
+        loop:true,
+        nav:false,
+        dots:true,
+        responsive:{
+            0: {
+                items: 1
+            },
+            600: {
+                items: 3
+            },
+            1000: {
+                items: 5
+            }
+        }
+    });
+
+    //blogs owl carousel
+    $("#blogs .owl-carousel").owlCarousel({
+        loop:true,
+        nav:false,
+        dots:true,
+        responsive:{
+            0: {
+                items: 1
+            },
+            600: {
+                items: 3
+            }
+        }
+    });
 })
